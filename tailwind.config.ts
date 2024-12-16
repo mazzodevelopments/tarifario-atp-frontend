6154e88238af7810a0f71c2f556f6e102beafd60
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-12-16 09:12:21 -0300

    Paleta de colores

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,9 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#2795d5",
+        secondary: "#254460",
+        background: "#ffffff",
       },
     },
   },
