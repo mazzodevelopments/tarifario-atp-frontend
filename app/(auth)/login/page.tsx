a63041da1c4e201fcce2bf9fee8765b2727d4d3b
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-12-16 15:51:17 -0300

    Login

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import Button from "@/app/components/Button";
+
+const inputStyles =
+  "w-full rounded-lg h-12 bg-neutral-100 border-[1.5px] p-4 focus:outline-primary";
+
+export default function Register() {
+  const handleSubmit = () => {
+    console.log("Submitted");
+  };
+  return (
+    <div className="w-screen h-screen justify-center items-center flex bg-secondary">
+      <form
+        onSubmit={handleSubmit}
+        className="w-1/4 h-auto bg-white rounded-2xl flex justify-start p-10 flex-col shadow-2xl gap-4"
+      >
+        <h2 className="font-bold text-4xl">Log In</h2>
+        <div>
+          <span className="font-semibold">Correo electrónico</span>
+          <input className={inputStyles} />
+        </div>
+        <div>
+          <span className="font-semibold">Contraseña</span>
+          <input className={inputStyles}></input>
+        </div>
+        <div>
+          <Button className="h-12 w-full" variant="primary" type="submit">
+            Log In
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
