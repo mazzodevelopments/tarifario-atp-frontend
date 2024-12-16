6083ddacae8a3c0cfe3768e83009fd717abbc5d5
Monza <matiasmonzalvoarg@gmail.com>
2024-12-16 12:27:13 -0300

    reg

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,10 @@
 export default function Register() {
   return (
-    <div className="w-screen h-screen justify-center items-center flex">
-      <div className="w-1/3 h-4/5 bg-gray-100"></div>
+    <div className="w-screen h-screen justify-center items-center flex bg-secondary">
+      <div className="w-1/4 h-3/5 bg-white rounded-2xl flex justify-start p-10 flex-col shadow-2xl">
+        <h2 className="font-bold text-4xl">Registrarse</h2>
+        <input className="w-full mt-4 rounded-lg h-12 bg-neutral-100 border-[1.5px]"></input>
+      </div>
     </div>
   );
 }
