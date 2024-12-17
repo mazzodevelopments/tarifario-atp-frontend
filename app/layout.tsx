c385928043ba86cbcb9e1cc757d09214a4d8e63d
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-12-17 17:15:12 -0300

    Font Poppins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,10 @@
-import { Geist, Azeret_Mono as Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
 });
 
 export const metadata = {
@@ -23,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${poppins.variable} font-sans antialiased`}>
         {children}
       </body>
     </html>
