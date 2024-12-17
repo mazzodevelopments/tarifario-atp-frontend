05cead1b63148298ee7691ab260a0c1f2729c32f
Monza <matiasmonzalvoarg@gmail.com>
2024-12-17 17:23:22 -0300

    background white

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 export default function Dashboard() {
   return (
     <div className="flex justify-center items-center pt-24  w-full h-full">
-      <div className="w-full h-full bg-white"></div>
+      <div className="w-full h-full bg-background"></div>
     </div>
   );
 }
