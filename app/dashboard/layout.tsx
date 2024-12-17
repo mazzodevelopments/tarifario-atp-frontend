c12cb7c9d68fbf800800302f27f5623bb6da66fa
Monza <matiasmonzalvoarg@gmail.com>
2024-12-17 18:31:53 -0300

    sidebar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,13 @@
 import Image from "next/image";
+import {
+  Clock,
+  CornerLeftDown,
+  CornerRightUp,
+  FilePlus,
+  Home,
+  LogOut,
+  PlusSquare,
+} from "react-feather";
 
 export default function AuthLayout({
   children,
@@ -6,29 +15,41 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="w-screen relative h-screen bg-secondary flex justify-center items-center flex-col px-[4vw] py-[1vw]">
+    <div className="w-screen relative h-screen bg-secondary flex justify-center items-center flex-row px-[1vw] py-[1vw]">
+      <div className="w-[10vw] h-full flex justify-between items-center flex-col">
+        <div className="w-[4vw] h-28 justify-center items-center flex">
+          <Image src={"/logo-nav.png"} width={1000} height={1000} alt="LOGO" />
+        </div>
+        <div className="flex justify-center items-center gap-[3vw] flex-col">
+          <a className="text-white hover:text-primary hover:cursor-pointer">
+            <Home size={28} />
+          </a>
+          <a className="text-white hover:text-primary hover:cursor-pointer">
+            <PlusSquare size={28} />
+          </a>
+          <a className="text-white hover:text-primary hover:cursor-pointer">
+            <Clock size={28} />
+          </a>
+          <a className="text-white hover:text-primary hover:cursor-pointer">
+            <CornerLeftDown size={28} />
+            {/* Estos se cambian */}
+          </a>
+          <a className="text-white hover:text-primary hover:cursor-pointer">
+            <CornerRightUp size={28} />
+            {/* Estos se cambian */}
+          </a>
+        </div>
+        <div className="w-[5vw] h-28 justify-center items-center flex">
+          <a className="text-white hover:text-primary hover:cursor-pointer">
+            <LogOut size={28} />
+          </a>
+        </div>
+      </div>
       <div className="w-full h-full relative flex justify-center items-center flex-col rounded-2xl overflow-hidden">
-        <div className="flex w-[100%] px-[2%] h-28  bg-white justify-between items-center absolute top-0 left-0">
-          <div className="w-44">
-            <Image
-              src={"/logo-nav.png"}
-              width={1000}
-              height={1000}
-              alt="LOGO"
-            />
-          </div>
-          <div className="flex justify-center items-center gap-8">
-            <a className="text-neutral-900 hover:text-primary hover:cursor-pointer">
-              Inicio
-            </a>
-            <a className="text-neutral-900 hover:text-primary hover:cursor-pointer">
-              Importar
-            </a>
-            <a className="text-neutral-900 hover:text-primary hover:cursor-pointer">
-              Exportar
-            </a>
+        <div className="flex w-[100%] px-[2%] h-28  bg-white justify-end items-center absolute top-0 left-0">
+          <div className="flex justify-end items-center gap-8">
             {/* DATOS USUARIO */}
-            <div className="flex justify-center items-center gap-2 h-14 px-2 rounded-2xl hover:bg-[#1E3A50] hover:cursor-pointer">
+            <div className="flex justify-end items-center gap-2 h-14 px-2 rounded-2xl hover:bg-[#1E3A50] hover:cursor-pointer">
               <div className="w-10 h-10 rounded-xl border-[0.5px] border-neutral-400 overflow-hidden">
                 <Image
                   src={"/DEFAULT_PROFILE_PIC.png"}
