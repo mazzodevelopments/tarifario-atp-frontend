15717a2516bb75df01ae7be9f7851a80abf753da
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-12-17 16:46:21 -0300

    Admin

---
+++ app/admin/page.tsx
@@ -0,0 +1,7 @@
+export default function Admin() {
+  return (
+    <div className="flex justify-center items-center w-screen h-full bg-background">
+      <div className="w-[80%]">ADMIN</div>
+    </div>
+  );
+}
