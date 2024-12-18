c5ada435c576e9c6023b953f3aa14e6b53e08f43
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-12-18 11:35:48 -0300

    404

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { useRouter } from "next/navigation";
+import Button from "@/components/Button";
+
+export default function NotFound() {
+  const router = useRouter();
+
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen bg-background gap-4">
+      <h1 className="text-6xl font-bold">404</h1>
+      <p className="text-xl">Ups! La página no existe.</p>
+      <Button onClick={() => router.push("/")} className="mt-2 p-2">
+        Volver a inicio
+      </Button>
+    </div>
+  );
+}
