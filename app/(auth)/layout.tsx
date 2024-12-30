af822a37d43948590533afbb27c4b5a6b15be9af
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-12-30 09:58:59 -0300

    Logo

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -10,9 +10,9 @@ export default function AuthLayout({
       <div className="w-[50%] flex justify-end px-[2%] items-center h-full">
         <div className="w-[18vw] mt-[0.75vw] h-auto pr-20 border-r-2 border-r-gray-300 flex flex-col justify-center items-center">
           <Image src={"/logo-nav.png"} width={1000} height={1000} alt="LOGO" />
-          <h1 className="flex items-center justify-center mt-6 text-3xl font-bold">
-            <span className="text-primary">atp</span>
-            <span className="text-gray-800">SOLUTIONS</span>
+          <h1 className="flex items-center justify-center mt-6 text-3xl ">
+            <span className="font-bold text-primary">atp</span>
+            <span className="font-semibold text-gray-800">SOLUTIONS</span>
           </h1>
         </div>
       </div>
