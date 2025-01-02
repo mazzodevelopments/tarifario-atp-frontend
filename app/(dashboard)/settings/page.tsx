32ddd3ef3974c122d1b8e2962566a8c63658b1e0
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2025-01-02 10:04:15 -0300

    Settings v2

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -4,6 +4,49 @@ import defaultProfilePic from "@/public/default-profile-pic.png";
 import Button from "@/components/Button";
 
 export default function Reports() {
+  const inputFields = [
+    { label: "Nombre", type: "text", value: "Matías Monzalvo" },
+    {
+      label: "Email",
+      type: "email",
+      value: "matiasmonzalvo@mazzodevelopments.com",
+    },
+    { label: "Teléfono", type: "tel", value: "+542944723412" },
+    {
+      label: "Dirección",
+      type: "text",
+      value: "Soldado de la Independencia 1468",
+    },
+  ];
+
+  const EditableInput = ({
+    label,
+    type,
+    value,
+  }: {
+    label: string;
+    type: string;
+    value: string;
+  }) => (
+    <div className="w-full mt-4 flex flex-col">
+      <label className="text-sm font-semibold text-gray-700">{label}</label>
+      <div className="relative w-full p-1 border-b-2 border-gray-100">
+        <input
+          type={type}
+          value={value}
+          className="w-full pr-1.5 pt-2.5 font-medium text-gray-600 text-sm outline-none"
+          readOnly
+        />
+        <Button
+          className="absolute right-0 top-0 text-xs rounded-xl px-3.5 py-2"
+          variant="primary"
+        >
+          Editar
+        </Button>
+      </div>
+    </div>
+  );
+
   return (
     <div className="flex flex-col justify-start w-full h-full bg-background py-8 px-6">
       <div className="flex mb-4 items-center">
@@ -12,106 +55,45 @@ export default function Reports() {
         </h2>
       </div>
       <div className="flex flex-row w-full h-full justify-start items-start gap-2 bg-white rounded-[40px] px-2 pb-2 border-2 border-gray-100">
-        <div className="flex flex-col w-full p-4">
+        <div className="flex flex-col w-[60%] p-4">
           <label className="text-md font-semibold text-gray-700 mb-2">
-            Foto de perfil
+            Mi cuenta
           </label>
-          <div className="flex justify-start items-center">
-            <Image
-              className="w-24 h-24 rounded-[28px]"
-              src={defaultProfilePic.src}
-              alt="Settings"
-              width={200}
-              height={200}
-            />
-            <div className="flex flex-col gap-2 ml-4">
-              <Button className="flex items-center text-xs w-full rounded-xl px-3.5 py-2">
-                <UploadCloud className="mr-2" size={20} />
-                Nueva foto de perfil
-              </Button>
+          <div className="flex w-full justify-between items-center pb-4 border-b-2 border-gray-100">
+            <div className="flex gap-4 items-center">
+              <Image
+                className="w-20 h-20 rounded-[28px]"
+                src={defaultProfilePic.src}
+                alt="Settings"
+                width={80}
+                height={80}
+              />
+              <div className="flex flex-col w-[60%]">
+                <h3 className="font-semibold text-sm text-gray-600">
+                  Sube una foto de perfil
+                </h3>
+                <label className="font-medium text-sm text-gray-500">
+                  Así tu equipo te reconocerá en cada etapa.
+                </label>
+              </div>
+            </div>
+            <div className="flex gap-2 ml-4">
               <Button
                 variant="outline"
-                className="text-xs rounded-xl px-3.5 py-2"
+                className="text-xs rounded-xl px-3.5 py-1.5"
               >
                 Eliminar
               </Button>
-            </div>
-          </div>
-          <div className="w-full mt-6 flex flex-col">
-            <label className="text-sm font-semibold text-gray-700 mb-2">
-              Nombre
-            </label>
-            <div className="relative w-full p-1 rounded-xl border-2 border-gray-100">
-              <input
-                type="name"
-                value="Matías Monzalvo"
-                className="w-full p-1.5 font-medium text-gray-600 text-sm outline-none"
-                readOnly
-              />
-              <Button
-                className="absolute right-0 top-0 bottom-0 text-xs rounded-l-none rounded-xl px-2"
-                variant="primary"
-              >
-                Modificar
+              <Button className="flex items-center text-xs w-full rounded-xl px-3.5 py-1.5">
+                <UploadCloud className="mr-2" size={20} />
+                Nueva foto de perfil
               </Button>
             </div>
           </div>
-          <div className="w-full mt-4 flex flex-col">
-            <label className="text-sm font-semibold text-gray-700 mb-2">
-              Email
-            </label>
-            <div className="relative w-full p-1 rounded-xl border-2 border-gray-100">
-              <input
-                type="email"
-                value="matiasmonzalvo@mazzodevelopments.com"
-                className="w-full p-1.5 font-medium text-gray-600 text-sm outline-none"
-                readOnly
-              />
-              <Button
-                className="absolute right-0 top-0 bottom-0 text-xs rounded-l-none rounded-xl px-2"
-                variant="primary"
-              >
-                Modificar
-              </Button>
-            </div>
-          </div>
-          <div className="w-full mt-4 flex flex-col">
-            <label className="text-sm font-semibold text-gray-700 mb-2">
-              Teléfono
-            </label>
-            <div className="relative w-full p-1 rounded-xl border-2 border-gray-100">
-              <input
-                type="phone"
-                value="+542944723412"
-                className="w-full p-1.5 font-medium text-gray-600 text-sm outline-none"
-                readOnly
-              />
-              <Button
-                className="absolute right-0 top-0 bottom-0 text-xs rounded-l-none rounded-xl px-2"
-                variant="primary"
-              >
-                Modificar
-              </Button>
-            </div>
-          </div>
-          <div className="w-full mt-4 flex flex-col">
-            <label className="text-sm font-semibold text-gray-700 mb-2">
-              Dirección
-            </label>
-            <div className="relative w-full p-1 rounded-xl border-2 border-gray-100">
-              <input
-                type="address"
-                value="Soldado de la Independencia 1468"
-                className="w-full p-1.5 font-medium text-gray-600 text-sm outline-none"
-                readOnly
-              />
-              <Button
-                className="absolute right-0 top-0 bottom-0 text-xs rounded-l-none rounded-xl px-2"
-                variant="primary"
-              >
-                Modificar
-              </Button>
-            </div>
+          <div className="w-full">
+            {inputFields.map((field, index) => (
+              <EditableInput key={index} {...field} />
+            ))}
           </div>
         </div>
       </div>
