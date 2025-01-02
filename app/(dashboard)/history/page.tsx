b82a655c1abc129fc07ecb4ea566c8785262e699
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2025-01-02 10:43:22 -0300

    Header v2

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -44,25 +44,25 @@ const cotizaciones = [
 export default function History() {
   return (
     <div className="flex justify-start w-full h-full flex-col bg-background p-[20px]">
-      <div className="w-full h-36 rounded-[10px]  mb-4 bg-gradient-to-r from-primary to-sky-200">
-        <div className="flex flex-col justify-end items-start h-full px-4 py-6">
-          <h2 className="text-[2vw] leading-[1] font-semibold text-white">
+      <div className="w-full h-32 rounded-[10px] mb-4 bg-gradient-to-r from-primary to-sky-200">
+        <div className="flex flex-col justify-end items-start h-full p-4">
+          <h2 className="text-[1.8vw] leading-[0.75] font-semibold text-white">
             Cotizaciones
           </h2>
-          <p className="text-[0.85vw] font-regular leading-[1] ml-1 text-white mt-2 opacity-90">
+          <p className="text-[0.85vw] leading-[1] ml-1 text-white mt-2 opacity-90">
             Historial de cotizaciones realizadas
           </p>
         </div>
       </div>
       <div className="flex mb-4 items-center ">
-        <div className="rounded-[10px] flex bg-sky-50 h-10 px-4 items-center justify-start">
+        <div className="w-[30%] rounded-[10px] flex bg-sky-50 h-8 px-4 items-center justify-start">
           <Search
             className="text-primary mr-2 opacity-50"
             fontWeight="bold"
             size={20}
           />
           <input
-            className="text-lg font-regular text-primary bg-transparent outline-none placeholder:text-primary placeholder:opacity-50"
+            className="text-sm font-regular text-primary bg-transparent outline-none placeholder:text-primary placeholder:opacity-50"
             placeholder="Buscar cotización"
           />
         </div>
