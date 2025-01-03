5c4f28f92c21128708626a493b53d0c732f0be0b
Monza <matiasmonzalvoarg@gmail.com>
2025-01-03 09:07:47 -0300

    headers

--- app/(dashboard)/create/page.tsx
+++ app/(dashboard)/create/page.tsx
@@ -1,10 +1,20 @@
+import { Plus } from "react-feather";
+
 export default function Create() {
   return (
-    <div className="flex justify-start w-full h-full flex-col bg-background py-8 px-6">
-      <div className="flex mb-4 items-center text-gray-900">
-        <h2 className="font-semibold text-3xl mt-[1px] ml-2">
-          Agregar Cotización
-        </h2>
+    <div className="flex justify-start w-full h-full flex-col bg-background p-[20px]">
+      <div className="w-full h-full flex justify-center items-center rounded-[10px]  bg-secondary">
+        <div className="flex flex-col justify-center w-full items-center h-full p-4">
+          <div className="bg-slate-800 rounded-full p-6">
+            <Plus size={48} className="text-white " />
+          </div>
+          <h2 className="text-[2vw] leading-[0.85] font-semibold text-white">
+            Crear cotización
+          </h2>
+          <p className="text-[0.8vw] leading-[1] ml-1 text-white mt-2 opacity-90">
+            Lista de proveedores oficiales
+          </p>
+        </div>
       </div>
     </div>
   );
--- app/(dashboard)/proveedores/page.tsx
+++ app/(dashboard)/proveedores/page.tsx
@@ -1,8 +1,15 @@
 export default function Proveedores() {
   return (
-    <div className="flex justify-start w-full h-full flex-col bg-background py-8 px-6">
-      <div className="flex mb-4 items-center text-gray-900">
-        <h2 className="font-semibold text-3xl mt-[1px] ml-2">Proveedores</h2>
+    <div className="flex justify-start w-full h-full flex-col bg-background p-[20px]">
+      <div className="w-full h-[7vw] rounded-[10px] mb-4 bg-gradient-to-r from-red-500 to-orange-300">
+        <div className="flex flex-col justify-end items-start h-full p-4">
+          <h2 className="text-[2vw] leading-[0.85] font-semibold text-white">
+            Proveedores
+          </h2>
+          <p className="text-[0.8vw] leading-[1] ml-1 text-white mt-2 opacity-90">
+            Lista de proveedores oficiales
+          </p>
+        </div>
       </div>
     </div>
   );
