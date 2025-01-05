5ffe4228abcb6e4d80d6f3b66744ae71996d160c
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2025-01-05 17:27:41 -0300

    Steps

--- app/(dashboard)/create/StepContent.tsx
+++ app/(dashboard)/create/StepContent.tsx
@@ -1,13 +1,25 @@
+import IncotermSelect from "@/app/(dashboard)/create/steps/IncotermSelect";
+
 interface StepContentProps {
   step: number;
 }
 
 export default function StepContent({ step }: StepContentProps) {
+  const renderStep = () => {
+    switch (step) {
+      case 0:
+        return <IncotermSelect />;
+      default:
+        return <p>Contenido de la etapa {step}</p>;
+    }
+  };
+
   return (
-    <div className="flex-grow">
-      <h3 className="text-xl font-semibold mb-4">Etapa {step}</h3>
-      {/* Aquí puedes agregar el contenido específico de cada etapa */}
-      <p>Contenido de la etapa {step}</p>
+    <div className="flex-grow flex flex-col">
+      <h3 className="text-xl font-semibold mb-4">Etapa {step + 1}</h3>
+      <div className="flex justify-center items-center w-full mx-auto">
+        {renderStep()}
+      </div>
     </div>
   );
 }
---
+++ app/(dashboard)/create/steps/IncotermSelect.tsx
@@ -0,0 +1,33 @@
+export default function IncotermSelect() {
+  const incoterms = [
+    { value: "EXW", label: "EXW - Ex Works" },
+    { value: "FCA", label: "FCA - Free Carrier" },
+    { value: "CPT", label: "CPT - Carriage Paid To" },
+    { value: "CIP", label: "CIP - Carriage and Insurance Paid To" },
+    { value: "DAP", label: "DAP - Delivered At Place" },
+    { value: "DPU", label: "DPU - Delivered at Place Unloaded" },
+    { value: "DDP", label: "DDP - Delivered Duty Paid" },
+    { value: "FAS", label: "FAS - Free Alongside Ship" },
+    { value: "FOB", label: "FOB - Free On Board" },
+    { value: "CFR", label: "CFR - Cost and Freight" },
+    { value: "CIF", label: "CIF - Cost, Insurance, and Freight" },
+  ];
+
+  return (
+    <div className="flex flex-col items-center justify-center p-4">
+      <h2 className="text-center text-xl font-medium text-secondary mb-4">
+        Seleccionar que Incoterm desea cotizar
+      </h2>
+      <select className="w-full p-2 border-2 border-gray-200 rounded-md text-sm focus:outline-none">
+        <option value="" disabled>
+          Seleccione un Incoterm
+        </option>
+        {incoterms.map((incoterm) => (
+          <option key={incoterm.value} value={incoterm.value}>
+            {incoterm.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
