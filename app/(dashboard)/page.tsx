d41ce99ed32403475289eff7b010baf42db3f2ce
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2025-01-06 11:01:29 -0300

    Home con header

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Plus } from "react-feather";
+import { ChevronRight, Plus } from "react-feather";
 import { User, Calendar, DollarSign } from "react-feather";
 
 export default function Dashboard() {
@@ -31,17 +31,37 @@ export default function Dashboard() {
 
   return (
     <div className="flex justify-start w-full h-full flex-col bg-background py-8 px-6">
+      <div className="w-full h-[7vw] rounded-[10px] mb-4 bg-gradient-to-r from-primary to-sky-200 flex-shrink-0">
+        <div className="flex flex-col justify-end items-start h-full p-4">
+          <h2 className="text-[2vw] leading-[0.85] font-semibold text-white">
+            General
+          </h2>
+          <p className="text-[0.8vw] leading-[1] ml-1 text-white mt-2 opacity-90">
+            Vista general del tarifario
+          </p>
+        </div>
+      </div>
       <div className="flex gap-4 mb-4 items-start text-gray-900">
         {/* CREAR COTIZACION */}
-        <div className="flex flex-col w-1/2 h-[14vw] rounded-[10px] mb-4 bg-gray-100 p-4">
-          <h2 className="text-lg font-semibold text-secondary mb-2">
-            Crear Cotización
-          </h2>
+        <div className="flex flex-col justify-between w-1/2 h-[14vw] rounded-[10px] mb-4 bg-gray-100 p-4">
+          <div>
+            <h2 className="text-2xl font-semibold text-secondary mb-2">
+              Crear Cotización
+            </h2>
+            <p className="text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac
+              leo vitae felis bibendum varius at non quam. Pellentesque ornare
+              porttitor purus, vitae elementum est lobortis fermentum. Integer
+              ac velit ut tellus fringilla pellentesque. Vestibulum vel felis
+              nisl. Nam molestie eu est nec accumsan.
+            </p>
+          </div>
           <button
             onClick={handleNavigateToCreate}
-            className="flex h-full items-center justify-center rounded-lg gap-2 p-2 bg-background"
+            className="flex items-center justify-between rounded-lg gap-2 p-2 bg-background"
           >
-            <Plus size={50} className="text-secondary" />
+            <label className="pl-4 font-medium">Cotizar</label>
+            <ChevronRight size={30} className="text-secondary" />
           </button>
         </div>
 
