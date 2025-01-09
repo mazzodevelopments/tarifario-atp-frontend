e1d31f7cfa023ba2d24e877c3cd1ddb07c76ff0c
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2025-01-09 18:11:06 -0300

    Sidebar secondary

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -46,27 +46,27 @@ export default function AuthLayout({
   return (
     <div className="w-screen relative h-screen flex justify-center items-center flex-row">
       <div className="w-full h-full flex justify-center items-center flex-row overflow-hidden py-4 pl-4">
-        <div className="w-[328px] h-full flex justify-between items-center flex-col bg-sky-50 z-20 p-[20px] pt-10 rounded-[18px]">
+        <div className="w-[328px] h-full flex justify-between items-center flex-col bg-secondary z-20 p-[20px] pt-10 rounded-[18px]">
           <div className="flex justify-start w-full items-start flex-col">
             <div className="w-full flex flex-row items-end pb-6">
               <h3 className="text-xl font-bold text-primary leading-[1]">
                 atp
               </h3>
-              <h3 className="text-lg font-semibold uppercase leading-[1] text-gray-900">
+              <h3 className="text-lg font-semibold uppercase leading-[1] text-background">
                 solutions
               </h3>
             </div>
             <div className="w-full flex justify-between mt-2 items-start gap-2 flex-col">
-              <label className="uppercase text-gray-400 text-xs font-medium">
+              <label className="uppercase text-gray-200 text-xs font-medium">
                 General
               </label>
               {menuItems.map((item) => (
                 <Link
                   key={item.id}
-                  className={`text-sm text-black flex items-center justify-between hover:cursor-pointer transition-all duration-300 ease-in-out w-full font-medium py-1.5 rounded-lg px-2 ${
+                  className={`text-sm flex items-center justify-between hover:cursor-pointer transition-all duration-300 ease-in-out w-full font-medium py-1.5 rounded-lg px-2 ${
                     selectedItem === item.id
-                      ? " bg-sky-100"
-                      : "hover:bg-gray-100 opacity-55"
+                      ? " bg-primary text-background"
+                      : "hover:bg-secondaryLight text-gray-200"
                   }`}
                   href={`/${item.id}`}
                 >
@@ -75,7 +75,7 @@ export default function AuthLayout({
                       size={18}
                       fontWeight="bold"
                       className={`transition-all duration-300 ${
-                        selectedItem === item.id ? " text-primary" : ""
+                        selectedItem === item.id ? " text-white" : ""
                       }`}
                     />
                     <span className="text-md ml-2 flex">{item.label}</span>
@@ -89,7 +89,7 @@ export default function AuthLayout({
           </div>
           <div className="w-full flex flex-col justify-end">
             <div className="flex w-full h-auto justify-start items-center">
-              <div className="flex flex-col gap-2 items-center py-2 px-4 rounded-[18px] w-full text-gray-800 bg-sky-100">
+              <div className="flex flex-col gap-2 items-center py-2 px-4 rounded-[18px] w-full text-gray-800 bg-primary">
                 {/* DATOS USUARIO */}
                 <div className="flex w-full items-center gap-2 h-14 hover:cursor-pointer">
                   <div className="w-8 h-8 rounded-xl overflow-hidden">
@@ -101,23 +101,23 @@ export default function AuthLayout({
                     />
                   </div>
                   <div className="flex flex-col">
-                    <h3 className="text-sm font-semibold w-full text-primary">
+                    <h3 className="text-sm font-semibold w-full text-background">
                       Tomás Matteozzi
                     </h3>
-                    <span className="text-[0.65vw] text-gray-600">
+                    <span className="text-[0.65vw] text-background">
                       Administrador
                     </span>
                   </div>
                 </div>
-                <div className="flex justify-between w-full gap-2">
+                <div className="flex justify-between w-full gap-2 text-background">
                   <Link
-                    className="flex w-1/2 gap-2 py-2 items-center text-xs hover:text-primary"
+                    className="flex w-1/2 gap-2 py-2 items-center text-xs hover:text-secondary"
                     href="/settings"
                   >
                     <Settings size={18} fontWeight="bold" /> Ajustes
                   </Link>
                   <Link
-                    className="flex w-1/2 gap-2 py-2 items-center text-xs hover:text-primary"
+                    className="flex w-1/2 gap-2 py-2 items-center text-xs hover:text-secondary"
                     href="/logout"
                   >
                     <LogOut size={18} fontWeight="bold" /> Log out
