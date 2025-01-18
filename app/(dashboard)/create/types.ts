123100708e86bf510775b0f45d0bd15ce4b33071
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2025-01-18 10:23:52 -0300

    Transport types

--- app/(dashboard)/create/types.ts
+++ app/(dashboard)/create/types.ts
@@ -33,3 +33,42 @@ export interface Custom {
   seguridadElectrica: number;
   senasa: number;
 }
+
+export interface Transport {
+  id: string;
+  via: string;
+  pickUp: boolean;
+  pickUpPrice: number;
+  repackaging: boolean;
+  fumigation: boolean;
+}
+
+export interface AirOrCourier extends Transport {
+  edcadassa: number; // ESTADÍA EN EDCADASSA SON USD 70 X DÍA
+  internationalFreight: number;
+  internationalInsurance: number; // PORCENTAJE DESDE 0,4% a 1%
+  administrativeCharges: number;
+  airWayBillCut: number;
+}
+
+export interface SeaOrLand extends Transport {
+  // PUERTO
+  transferToCustomsWarehouse: number;
+  deconsolidation: number;
+  movement: number;
+  administrativeCharges: number;
+  electronicSeal: number;
+  emptyContainerReturnBeforeDeadline: number;
+  lateReturnFee: number; // 12 USD X DÍA DESDE EL PRIMER DÍA
+  storageDays: number;
+  containerCleaning: number;
+  optionalCustody: number;
+  senasaVerification: number;
+  //FORDWARDER
+  originExpenses: number;
+  internationalFreight: number;
+  generalPortAdministration: number;
+  localExpenses: number; //
+  deconsolidation: number; //
+  blPaymentForOriginalDocumentRetrieval: number;
+}
