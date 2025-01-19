81269e38cd73130e5cc40b95d5f80e7c2081288c
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2025-01-19 12:23:19 -0300

    Custom types

--- app/(dashboard)/create/types.ts
+++ app/(dashboard)/create/types.ts
@@ -72,22 +72,24 @@ export interface ForwarderInternationalFreight {
 
 /*** ADUANA ***/
 export interface Custom {
+  id: string;
   sediLegalizationFee: number; // USD 50
+  // CALCULO DE BASE IMPONIBLE
   invoiceValueFOB: number; //
   internationalFreightValue: number; // VALOR DEL PASO ANTERIOR
-  insuranceValue: number; // 1%
-  taxableBase: number; // BASE IMPONIBLE
-  importDutyRate: number; // DERECHOS DE IMPORTACIÓN
-  statisticsRate: number; // 3%
-  ivaBase: number; // DERECHOS + ESTADISITCAS
-  ivaRate: number; // VAT rate (21%)
-  additionalIvaRate: number; // Additional VAT rate (10.5%)
-  incomeTaxRate: number; // 6%
-  grossIncomeRate: number; // 2.5%
+  taxableBase: number; // BASE IMPONIBLE --> FOB + Flete Internacional + (FOB x 0.01)
+  // IMPUESTOS
+  importDutyRate: number; // DERECHOS DE IMPORTACIÓN (Varía según la mercaderia y el arancel)
+  statisticsRate: number; // ESTADISTICAS = BASE IMPONIBLE x 0.03
+  ivaRate: number; // IVA = (DERECHOS DE IMPORTACION + ESTADISITCA) x 0.21
+  additionalIvaRate: number; // IVA ADICIONAL = (DERECHOS DE IMPORTACION + ESTADISITCA) x 0.105
+  incomeTaxRate: number; // GANANCIAS = BASE IMPONIBLE x 0.06
+  grossIncomeRate: number; // INGRESOS BRUTOS = BASE IMPONIBLE x 0.025
   simFee: number; // USD 10
-  minimumCustomsDispatchCost: number; // Minimum customs dispatch cost (USD 250 or 0.8% of CIF value)
+  // OTROS GASTOS
   cifValue: number; // CIF value (INVOICE + INTERNATIONAL FREIGHT + INSURANCE)
-  customsOperationalCharges: number; // Operational charges (USD 210)
+  minimumCustomsDispatchCost: number; // PARA GASTOS DESPACHOS ADUANEROS SE USA EL MAX DE HONORARIOS MINIMOS (250 USD) O EL 0.8% DEL VALOR DEL CIF
+  customsOperationalCharges: number; // USD 210
   optionalElectricalSecurity: number; //  USD 150
   optionalSenasaFee: number; // USD 50
 }
