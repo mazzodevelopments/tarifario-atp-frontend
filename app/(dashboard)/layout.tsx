ceee174de2eed2dd085d97020de2fed1dc853235
Monza <matiasmonzalvoarg@gmail.com>
2025-02-05 17:06:22 -0300

    AuthContext

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,8 +1,9 @@
+import type React from "react";
 import PageTransition from "@/components/PageTransition";
 import { AppSidebar } from "./components/AppSidebar";
-
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AnimatePresence } from "framer-motion";
+import { AuthProvider } from "@/context/AuthContext";
 
 export default function AuthLayout({
   children,
@@ -10,15 +11,17 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <div className="flex flex-1 flex-col gap-4 overflow-hidden bg-neutral-50">
-          <AnimatePresence mode="wait">
-            <PageTransition>{children}</PageTransition>
-          </AnimatePresence>
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+    <AuthProvider>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <div className="flex flex-1 flex-col gap-4 overflow-hidden bg-neutral-50">
+            <AnimatePresence mode="wait">
+              <PageTransition>{children}</PageTransition>
+            </AnimatePresence>
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    </AuthProvider>
   );
 }
