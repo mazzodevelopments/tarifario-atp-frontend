371a3e8d2d7ed4c177efb2dd17771a10e24a89bc
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2025-02-14 12:12:09 -0300

    AdaptDropdown

--- app/adapters/adaptToDropdown.ts
+++ app/adapters/adaptToDropdown.ts
@@ -1,33 +1,33 @@
 type Option = {
-  id: string;
+  id: number;
   name: string;
 };
 
-const adaptToDropdown = <T>(
+export const adaptToDropdown = <T>(
   data: T[],
   idKey: keyof T,
   nameKey: keyof T,
 ): Option[] => {
   return data.map((item) => ({
-    id: String(item[idKey]), // Convertimos a string por si viene como número
+    id: Number(item[idKey]), // Convertimos a string por si viene como número
     name: String(item[nameKey]),
   }));
 };
 
-//  **Ejemplo de uso**
-// const familias = [
-//   { codigo: 1, descripcion: "Familia A" },
-//   { codigo: 2, descripcion: "Familia B" },
-//   { codigo: 3, descripcion: "Familia C" },
-// ];
-//
-// const dropdownOptions = adaptToDropdown(familias, "codigo", "descripcion");
-//
-// console.log(dropdownOptions);
-/*
-[
-  { id: "1", name: "Familia A" },
-  { id: "2", name: "Familia B" },
-  { id: "3", name: "Familia C" },
-]
-*/
+/** Ejemplo de uso**
+ const familias = [
+ { codigo: 1, descripcion: "Familia A" },
+ { codigo: 2, descripcion: "Familia B" },
+ { codigo: 3, descripcion: "Familia C" },
+ ];
+
+ const dropdownOptions = adaptToDropdown(familias, "codigo", "descripcion");
+
+ console.log(dropdownOptions);
+
+ [
+ { id: "1", name: "Familia A" },
+ { id: "2", name: "Familia B" },
+ { id: "3", name: "Familia C" },
+ ]
+ **/
