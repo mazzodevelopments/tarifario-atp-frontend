d87630d30875b6cd0498374039c60dead37c73ef
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2025-02-20 12:58:26 -0300

    test

--- app/utils/config.ts
+++ app/utils/config.ts
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "https://apitarifario.mazzodevelopments.com";
+// export const API_BASE_URL = "https://apitarifario.mazzodevelopments.com";
+export const API_BASE_URL = "http://localhost:30000";
