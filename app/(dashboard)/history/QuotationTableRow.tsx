91300628ab0558f6ed999694dd17b6ace4133cfc
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2025-02-28 10:28:49 -0300

    QuotationTable

---
+++ app/(dashboard)/history/QuotationTableRow.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import Link from "next/link";
+import { Briefcase, User, Calendar } from "lucide-react";
+import { TableCell, TableRow } from "@/components/ui/table";
+import Button from "@/components/Button";
+import type { HistoryQuotationCard } from "@/types/Quotations";
+
+type StepKeys = 1 | 2 | 3 | 4 | 5 | 6 | 7;
+
+const stepTexts: Record<StepKeys, string> = {
+  1: "Items",
+  2: "Compras",
+  3: "Logística",
+  4: "Ventas",
+  5: "Presupuestos",
+  6: "Selección",
+  7: "Completada",
+};
+
+export function QuotationTableRow({
+  quotation,
+  isPending = false,
+}: {
+  quotation: HistoryQuotationCard;
+  isPending?: boolean;
+}) {
+  const stepLinks: Record<StepKeys, string> = {
+    1: `/create/${quotation.id}/items`,
+    2: `/create/${quotation.id}/purchase-data`,
+    3: `/create/${quotation.id}/logistic`,
+    4: `/create/${quotation.id}/sales-data`,
+    5: `/create/${quotation.id}/select-budgets`,
+    6: `/create/${quotation.id}/review`,
+    7: `/history/${quotation.taskNumber}`,
+  };
+
+  const actualStep = quotation.step as StepKeys;
+  const stepText = stepTexts[actualStep] || "Estado desconocido";
+  const isCompleted = stepText === "Completada";
+  const bgColor = isCompleted ? "bg-green-100" : "bg-orange-100";
+  const textColor = isCompleted ? "text-green-600" : "text-orange-600";
+
+  return (
+    <TableRow className="text-sm text-center hover:bg-transparent">
+      <TableCell className="font-[600] text-black">
+        {quotation.taskNumber}
+      </TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2 justify-center">
+          <Briefcase className="text-gray-400 flex-shrink-0" size={16} />
+          <span>{quotation.client}</span>
+        </div>
+      </TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2 justify-center">
+          <User className="text-gray-400 flex-shrink-0" size={16} />
+          <span>{quotation.buyer}</span>
+        </div>
+      </TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2 justify-center">
+          <Calendar className="text-gray-400 flex-shrink-0" size={16} />
+          <span>
+            {new Date(quotation.expirationDateTime).toLocaleDateString("es-ES")}
+          </span>
+        </div>
+      </TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2 justify-center">
+          <Calendar className="text-gray-400 flex-shrink-0" size={16} />
+          <span>
+            {new Date(quotation.expirationDateTime).toLocaleDateString("es-ES")}
+          </span>
+        </div>
+      </TableCell>
+      {isPending && (
+        <TableCell>
+          <div className={`py-1 px-3 ${bgColor} rounded-3xl inline-block`}>
+            <span className={`text-sm ${textColor} font-[600]`}>
+              {stepText}
+            </span>
+          </div>
+        </TableCell>
+      )}
+      <TableCell>
+        <div className="flex items-center gap-2 justify-center">
+          <Link href={stepLinks[actualStep] || ""} passHref>
+            <Button
+              variant="secondary"
+              className="p-1 h-auto hover:bg-gray-100"
+            >
+              {isCompleted ? "Ver Detalles" : "Ir a cotizar"}
+            </Button>
+          </Link>
+        </div>
+      </TableCell>
+    </TableRow>
+  );
+}
--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -1,25 +1,37 @@
 "use client";
 
-import { Search } from "react-feather";
-import QuotationCard from "./QuotationCard";
 import { useEffect, useState } from "react";
+import { Search } from "react-feather";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { QuotationTableRow } from "@/app/(dashboard)/history/QuotationTableRow";
 import { QuotationsService } from "@/services/QuotationsService";
-import { HistoryQuotationCard } from "@/types/Quotations";
+import type { HistoryQuotationCard } from "@/types/Quotations";
 
 export default function History() {
-  const [unfinishedQuoations, setUnfinishedQuoations] = useState<
+  const [unfinishedQuotations, setUnfinishedQuotations] = useState<
     HistoryQuotationCard[]
   >([]);
-  const [finishedQuoations, setFinishedQuoations] = useState<
+  const [finishedQuotations, setFinishedQuotations] = useState<
     HistoryQuotationCard[]
   >([]);
+  const [activeTab, setActiveTab] = useState<"pending" | "completed">(
+    "pending",
+  );
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     const fetchUnfinishedQuotations = async () => {
       try {
         const unfinishedQuotations =
           await QuotationsService.getUnfinishedQuotations();
-        setUnfinishedQuoations(unfinishedQuotations);
+        setUnfinishedQuotations(unfinishedQuotations);
       } catch (error) {
         console.error("Error fetching quotation items:", error);
       }
@@ -27,9 +39,9 @@ export default function History() {
 
     const fetchFinishedQuotations = async () => {
       try {
-        const unfinishedQuotations =
+        const finishedQuotations =
           await QuotationsService.getFinishedQuotations();
-        setFinishedQuoations(unfinishedQuotations);
+        setFinishedQuotations(finishedQuotations);
       } catch (error) {
         console.error("Error fetching quotation items:", error);
       }
@@ -39,13 +51,12 @@ export default function History() {
     fetchFinishedQuotations();
   }, []);
 
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const filteredUnfinishedQuotations = unfinishedQuoations.filter((quotation) =>
-    quotation.taskNumber.toLowerCase().includes(searchTerm.toLowerCase()),
+  const filteredUnfinishedQuotations = unfinishedQuotations.filter(
+    (quotation) =>
+      quotation.taskNumber.toLowerCase().includes(searchTerm.toLowerCase()),
   );
 
-  const filteredFinishedQuotations = finishedQuoations.filter((quotation) =>
+  const filteredFinishedQuotations = finishedQuotations.filter((quotation) =>
     quotation.taskNumber.toLowerCase().includes(searchTerm.toLowerCase()),
   );
 
@@ -75,28 +86,101 @@ export default function History() {
         </div>
       </div>
       <div className="w-full px-6 pb-6 pt-4">
-        {unfinishedQuoations.length > 0 &&
-          filteredUnfinishedQuotations.length > 0 && (
-            <div className="w-full mb-4">
-              <h3 className="text-lg font-[600]">Pendientes</h3>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-start mt-2">
-                {filteredUnfinishedQuotations.map((quotation) => (
-                  <QuotationCard key={quotation.id} {...quotation} />
-                ))}
-              </div>
-            </div>
-          )}
-        {finishedQuoations.length > 0 &&
-          filteredFinishedQuotations.length > 0 && (
-            <div className="w-full">
-              <h3 className="text-lg font-[600]">Completadas</h3>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-start mt-2">
-                {filteredFinishedQuotations.map((quotation) => (
-                  <QuotationCard key={quotation.id} {...quotation} />
-                ))}
-              </div>
-            </div>
-          )}
+        <div className="flex space-x-4 mb-4">
+          <button
+            onClick={() => setActiveTab("pending")}
+            className={`py-2 px-4 rounded-lg font-semibold text-sm border ${
+              activeTab === "pending"
+                ? "bg-primary/5 text-primary"
+                : "bg-white text-gray-500  border-neutral-200"
+            }`}
+          >
+            Pendientes
+          </button>
+          <button
+            onClick={() => setActiveTab("completed")}
+            className={`py-2 px-4 rounded-lg font-semibold text-sm border ${
+              activeTab === "completed"
+                ? "bg-green-100 text-green-600"
+                : "bg-white text-gray-500 border-neutral-200"
+            }`}
+          >
+            Completadas
+          </button>
+        </div>
+
+        <div className="w-auto h-auto overflow-hidden rounded-[12px] shadow-sm shadow-cyan-500/20">
+          <div className="border rounded-[12px] overflow-auto max-h-[70vh] relative w-full">
+            <Table className="w-full">
+              <TableHeader>
+                <TableRow className="bg-primary/5">
+                  <TableHead className="text-primary font-[600] text-center">
+                    Número
+                  </TableHead>
+                  <TableHead className="text-primary font-[600] text-center">
+                    Cliente
+                  </TableHead>
+                  <TableHead className="text-primary font-[600] text-center">
+                    Comprador
+                  </TableHead>
+                  <TableHead className="text-primary font-[600] text-center">
+                    Fecha Recepción
+                  </TableHead>
+                  <TableHead className="text-primary font-[600] text-center">
+                    Fecha Expiración
+                  </TableHead>
+                  {activeTab === "pending" && (
+                    <TableHead className="text-primary font-[600] text-center">
+                      Etapa
+                    </TableHead>
+                  )}
+                  <TableHead className="text-primary font-[600] text-center">
+                    Acciones
+                  </TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {activeTab === "pending" ? (
+                  filteredUnfinishedQuotations.length > 0 ? (
+                    filteredUnfinishedQuotations.map((quotation) => (
+                      <QuotationTableRow
+                        key={quotation.id}
+                        quotation={quotation}
+                        isPending={true}
+                      />
+                    ))
+                  ) : (
+                    <TableRow className="h-36">
+                      <TableCell
+                        colSpan={7}
+                        className="text-sm m-auto h-full text-center text-gray-500"
+                      >
+                        No hay cotizaciones pendientes
+                      </TableCell>
+                    </TableRow>
+                  )
+                ) : filteredFinishedQuotations.length > 0 ? (
+                  filteredFinishedQuotations.map((quotation) => (
+                    <QuotationTableRow
+                      key={quotation.id}
+                      quotation={quotation}
+                      isPending={false}
+                    />
+                  ))
+                ) : (
+                  <TableRow className="h-36">
+                    <TableCell
+                      colSpan={6}
+                      className="text-sm m-auto h-full text-center text-gray-500"
+                    >
+                      No hay cotizaciones completadas
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
       </div>
     </div>
   );
