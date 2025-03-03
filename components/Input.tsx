5b9ea1c8eb247577fab94ea8bea00bae2a4391d4
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2025-03-03 16:19:04 -0300

    Error

--- components/Input.tsx
+++ components/Input.tsx
@@ -7,6 +7,7 @@ interface BaseInputProps {
   label?: string;
   className?: string;
   isTextArea?: boolean;
+  error?: string;
 }
 
 type InputProps = BaseInputProps &
@@ -19,24 +20,30 @@ export default function Input({
   label,
   className = "",
   isTextArea = false,
+  error,
   ...props
 }: InputProps) {
   return (
     <div>
-      <label className="block text-sm font-[600] text-gray-700">{label}</label>
+      {label && (
+        <label className="block text-sm font-[600] text-gray-700">
+          {label}
+        </label>
+      )}
       {isTextArea ? (
         <textarea
           {...(props as TextareaHTMLAttributes<HTMLTextAreaElement>)}
           className={`w-full px-2 py-2 text-sm border rounded-md focus:outline-none resize-y min-h-[70px] 
-            ${className}`}
+            ${className} ${error ? "border-red-500" : ""}`}
         />
       ) : (
         <input
           {...(props as InputHTMLAttributes<HTMLInputElement>)}
           className={`w-full px-2 py-2 text-sm border rounded-md focus:outline-none 
-            ${className}`}
+            ${className} ${error ? "border-red-500" : ""}`}
         />
       )}
+      {error && <p className="text-red-500 text-xs mt-1">{error}</p>}
     </div>
   );
 }
